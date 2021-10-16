315d57d0354f2a11c37a8f22684fff4982a3778a
Portocomercio <programas.portocomercio@hotmail.com>
2021-10-16 11:43:11 -0500

    Lee los codigos qr correctamente y envia la informacion mediante socket.io

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express');
-require('dotenv').config()
 const app = express();
 const cors = require('cors')
+const http = require('http');
 
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server, {
+    cors: {
+        origin: "*"
+    }
+});
+
+require('dotenv').config()
 app.use(cors())
 
 const PORT = process.env.PORT || 6000;
@@ -14,8 +23,14 @@ var serial = new SerialPort(COM_PORT, {
     baudRate: 9600
 });
 
-app.listen(PORT, function () {
+server.listen(PORT, function () {
     console.log('La aplicación esta funcionando en la siguiente direccion: http://0.0.0.0:' + PORT + '!');
+});
+
+app.get('/', function (req, res) {
+
+    return res.send('Working');
+ 
 });
 
 serial.on('open',function() {
@@ -23,5 +38,15 @@ serial.on('open',function() {
 });
 
 serial.on('readable', function () {
-    console.log('Data:', serial.read().toString().substring(1))
+
+    let mensaje = serial.read().toString().substring(1)
+    console.log(mensaje)
+
+    io.emit("enviar mensaje", {
+        "mensaje" : mensaje
+    })
 })
+
+io.on('connection', (socket) => {
+    // console.log('a user connected');
+});
